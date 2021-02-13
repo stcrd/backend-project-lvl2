b6a46cd82ba9dbe1fb5af352c6684dd322d0134d
stcrd <nurzhan82@mail.ru>
2021-02-14 00:50:38 +0600

    update index.test.js

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -10,24 +10,24 @@ const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', 
 
 test('two non-empty jsons', () => {
   const expected = '{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}';
-  expect(genDiff(getFixturePath('both-not-empty1.json'), getFixturePath('both-not-empty2.json')))
+  expect(genDiff(getFixturePath('not-empty1.json'), getFixturePath('not-empty2.json')))
     .toEqual(expected);
 });
 
 test('empty source, non-empty target', () => {
   const expected = '{\n  + host: hexlet.io\n  + timeout: 20\n  + verbose: true\n}';
-  expect(genDiff(getFixturePath('empty-source1.json'), getFixturePath('empty-source2.json')))
+  expect(genDiff(getFixturePath('empty.json'), getFixturePath('not-empty2.json')))
     .toEqual(expected);
 });
 
 test('non-empty source, empty target', () => {
   const expected = '{\n  - host: hexlet.io\n  - timeout: 20\n  - verbose: true\n}';
-  expect(genDiff(getFixturePath('empty-target1.json'), getFixturePath('empty-target2.json')))
+  expect(genDiff(getFixturePath('not-empty2.json'), getFixturePath('empty.json')))
     .toEqual(expected);
 });
 
 test('both empty', () => {
   const expected = '{\n\n}';
-  expect(genDiff(getFixturePath('both-empty1.json'), getFixturePath('both-empty2.json')))
+  expect(genDiff(getFixturePath('empty.json'), getFixturePath('empty.json')))
     .toEqual(expected);
 });
