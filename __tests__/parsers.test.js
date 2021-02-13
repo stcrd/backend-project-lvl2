b41e5b773d17b4cf2c41dbf9855e0ce2f8176fbc
stcrd <nurzhan82@mail.ru>
2021-02-14 00:52:47 +0600

    add parsers.test.js

---
+++ __tests__/parsers.test.js
@@ -0,0 +1,20 @@
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { test, expect } from '@jest/globals';
+import parse from '../src/parsers.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+
+test('non-empty json', () => {
+  const expected = {
+    host: 'hexlet.io',
+    timeout: 50,
+    proxy: '123.234.53.22',
+    follow: false,
+  };
+  expect(parse(getFixturePath('not-empty1.json')))
+    .toEqual(expected);
+});
