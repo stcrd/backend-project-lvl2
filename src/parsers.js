9a70ae58ad343731b052bee0d0d92f7e216c3bd9
stcrd <nurzhan82@mail.ru>
2021-02-14 00:51:22 +0600

    update parsers.js

--- src/parsers.js
+++ src/parsers.js
@@ -2,12 +2,9 @@ import fs from 'fs';
 import path from 'path';
 import yaml from 'js-yaml';
 
-export default (filepath1, filepath2) => {
-  const isJson = path.extname(filepath1) === '.json';
-  const rawData1 = fs.readFileSync(path.resolve(filepath1));
-  const rawData2 = fs.readFileSync(path.resolve(filepath2));
-  const parsedData1 = isJson ? JSON.parse(rawData1) : yaml.load(rawData1);
-  const parsedData2 = isJson ? JSON.parse(rawData2) : yaml.load(rawData2);
-
-  return { parsedData1, parsedData2 };
+export default (filepath) => {
+  const isJson = path.extname(filepath) === '.json';
+  const rawData = fs.readFileSync(path.resolve(filepath));
+  const parsedData = isJson ? JSON.parse(rawData) : yaml.load(rawData);
+  return parsedData;
 };
