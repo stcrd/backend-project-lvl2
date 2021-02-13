6ece21d9b34cf18152092c066a3fb81c1fbbbecb
stcrd <nurzhan82@mail.ru>
2021-02-14 00:25:30 +0600

    remove file reading and parsing from index.js

--- index.js
+++ index.js
@@ -1,31 +1,27 @@
-import fs from 'fs';
 import _ from 'lodash';
-import path from 'path';
+import parse from './src/parsers.js';
 
 const genDiff = (filepath1, filepath2) => {
-  const rawJson1 = fs.readFileSync(path.resolve(filepath1));
-  const rawJson2 = fs.readFileSync(path.resolve(filepath2));
-  const json1 = JSON.parse(rawJson1);
-  const json2 = JSON.parse(rawJson2);
+  const { parsedData1, parseData2 } = parse(filepath1, filepath2);
 
   const getLine = (status, key) => {
     const prefixes = { noChange: '    ', deleted: '  - ', added: '  + ' };
-    const value = status === 'added' ? json2[key] : json1[key];
+    const value = status === 'added' ? parseData2[key] : parsedData1[key];
     return `${prefixes[status]}${key}: ${value}`;
   };
 
-  const allKeys = [...Object.keys(json1), ...Object.keys(json2)];
+  const allKeys = [...Object.keys(parsedData1), ...Object.keys(parseData2)];
   const sortedUniqKeys = _.uniq(allKeys.sort());
 
   const entries = sortedUniqKeys
     .reduce((acc, key) => {
-      if (key in json1 && key in json2) {
-        const newEntry = json1[key] === json2[key]
+      if (key in parsedData1 && key in parseData2) {
+        const newEntry = parsedData1[key] === parseData2[key]
           ? [getLine('noChange', key)]
           : [getLine('deleted', key), getLine('added', key)];
         return [...acc, ...newEntry];
       }
-      const newEntry = key in json1 ? [getLine('deleted', key)] : [getLine('added', key)];
+      const newEntry = key in parsedData1 ? [getLine('deleted', key)] : [getLine('added', key)];
       return [...acc, ...newEntry];
     }, [])
     .join('\n');
