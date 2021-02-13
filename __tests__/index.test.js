fe14cdbf0bfed01f067d437677e8bb5b09ad4223
stcrd <nurzhan82@mail.ru>
2021-02-13 22:44:23 +0600

    update bunch of files

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -10,7 +10,7 @@ const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', 
 
 test('two non-empty jsons', () => {
   const expected = '{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}';
-  expect(genDiff(getFixturePath('1a.json'), getFixturePath('1b.json')))
+  expect(genDiff(getFixturePath('both-not-empty1.json'), getFixturePath('both-not-empty2.json')))
     .toEqual(expected);
 });
 
