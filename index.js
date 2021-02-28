201aab1bc17b4f65172ad3081d3793c29865ec75
stcrd <nurzhan82@mail.ru>
2021-02-28 19:28:25 +0600

    update index.js

--- index.js
+++ index.js
@@ -5,28 +5,27 @@ const genDiff = (filepath1, filepath2) => {
   const parsedData1 = parse(filepath1);
   const parsedData2 = parse(filepath2);
 
-  const getLine = (status, key) => {
-    const prefixes = { noChange: '    ', deleted: '  - ', added: '  + ' };
-    const value = status === 'added' ? parsedData2[key] : parsedData1[key];
-    return `${prefixes[status]}${key}: ${value}`;
-  };
+  const isObject = (value) => value instanceof Object && !Array.isArray(value);
 
-  const allKeys = [...Object.keys(parsedData1), ...Object.keys(parsedData2)];
-  const sortedUniqKeys = _.uniq(allKeys.sort());
-
-  const entries = sortedUniqKeys
-    .reduce((acc, key) => {
-      if (key in parsedData1 && key in parsedData2) {
-        const newEntry = parsedData1[key] === parsedData2[key]
-          ? [getLine('noChange', key)]
-          : [getLine('deleted', key), getLine('added', key)];
-        return [...acc, ...newEntry];
+  const generateDiff = (data1, data2) => {
+    const allKeys = (_.uniq([...Object.keys(data1), ...Object.keys(data2)])).sort();
+    const merged = allKeys.reduce((acc, key) => {
+      if (key in data1 && key in data2) {
+        if (data1[key] === data2[key]) {
+          return [...acc, { [key]: data1[key] }];
+        }
+        if (isObject(data1[key]) && isObject(data2[key])) {
+          return [...acc, { [key]: generateDiff(data1[key], data2[key]) }];
+        }
+        return [...acc, { [key]: data1[key], status: 'deleted' }, { [key]: data2[key], status: 'added' }];
       }
-      const newEntry = key in parsedData1 ? [getLine('deleted', key)] : [getLine('added', key)];
-      return [...acc, ...newEntry];
-    }, [])
-    .join('\n');
-
-  return `{\n${entries}\n}`;
+      if (key in data1) {
+        return [...acc, { [key]: data1[key], status: 'deleted' }];
+      }
+      return [...acc, { [key]: data2[key], status: 'added' }];
+    }, []);
+    return merged;
+  };
+  return generateDiff(parsedData1, parsedData2);
 };
 export default genDiff;
