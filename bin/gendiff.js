ab7586c22ac7c4549db768d9c6d63f002aa973ad
stcrd <nurzhan82@mail.ru>
2021-03-01 16:54:25 +0600

    add stylish as the default formater for executable

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,17 +1,20 @@
 #!/usr/bin/env node
 import commander from 'commander';
-import genDiff from '../index.js';
-import formater from '../src/stylish.js';
+import genDiff from '../src/generateDifference.js';
+import stylish from '../src/stylish.js';
 
 const { program } = commander;
 
 program
   .version('0.0.1')
   .description('Compares two configuration files and shows the difference.')
-  .option('-f, --format [type]', 'output format')
+  .option('-f, --format [type]', 'output format', 'stylish')
   .arguments('<filepath1> <filepath2>')
   .action((filepath1, filepath2) => {
-    const result = formater(genDiff(filepath1, filepath2));
+    const diff = genDiff(filepath1, filepath2);
+    const options = program.opts();
+    const formater = options.format === 'stylish' ? stylish : () => 'unknown formatting';
+    const result = formater(diff);
     console.log(result);
     return result;
   });
