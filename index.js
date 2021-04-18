d293992cb8bd282f5b7625516b9a4f1027a4df96
stcrd <nurzhan82@mail.ru>
2021-04-18 20:35:15 +0600

    add format selection to index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 import genDiff from './src/generateDifference.js';
-import defaultFormater from './src/default-formater.js';
+import defaultFormater from './src/formatters/default-formatter.js';
 
 export default (filepath1, filepath2) => defaultFormater(genDiff(filepath1, filepath2));
