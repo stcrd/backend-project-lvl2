bc168bb363e0c6434538228f10f80620adf0be98
stcrd <nurzhan82@mail.ru>
2021-04-18 23:42:04 +0600

    fix formatter selection in index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 import genDiff from './src/generateDifference.js';
-import defaultFormater from './src/formatters/default-formatter.js';
+import formatter from './src/formatters/index.js';
 
-export default (filepath1, filepath2) => defaultFormater(genDiff(filepath1, filepath2));
+export default (filepath1, filepath2) => formatter(genDiff(filepath1, filepath2));
