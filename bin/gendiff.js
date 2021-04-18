36d87d1d3e1679a241b8451c31117f8c449358b2
stcrd <nurzhan82@mail.ru>
2021-04-18 20:34:17 +0600

    add format selection feature

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,20 +1,20 @@
 #!/usr/bin/env node
 import commander from 'commander';
 import genDiff from '../src/generateDifference.js';
-import stylish from '../src/stylish.js';
+import formatSelector from '../src/formatters/index.js';
 
 const { program } = commander;
 
 program
   .version('0.0.1')
   .description('Compares two configuration files and shows the difference.')
-  .option('-f, --format [type]', 'output format', 'stylish')
+  .option('-f, --format [type]', 'output format', 'defaultFormatter')
   .arguments('<filepath1> <filepath2>')
   .action((filepath1, filepath2) => {
     const diff = genDiff(filepath1, filepath2);
     const options = program.opts();
-    const formater = options.format === 'stylish' ? stylish : () => 'unknown formatting';
-    const result = formater(diff);
+    const formatter = formatSelector(options.format);
+    const result = formatter(diff);
     console.log(result);
     return result;
   });
