2381cc1616666271a57847a15f704fa334add51e
stcrd <nurzhan82@mail.ru>
2021-04-18 21:17:28 +0600

    refactor stylish formatter

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,19 +1,18 @@
 import _ from 'lodash';
 
 const stylishFormatter = (diff, depth = 0) => {
+  const margin = '    '.repeat(depth);
   const mapped = diff.reduce((acc, el) => {
-    const [key] = Object.keys(el);
-    const value = el[key];
+    const [[key, value]] = Object.entries(el);
     const prefix = { deleted: '  - ', added: '  + ' }[el.status] || '    ';
-    if (Array.isArray(value)) {
-      return [...acc, `${'    '.repeat(depth)}${prefix}${key}: ${stylishFormatter(value, depth + 1)}`];
+    if (!(value instanceof Object)) {
+      return [...acc, `${margin}${prefix}${key}: ${value}`];
     }
-    if (value instanceof Object) {
-      const newValue = _.flatMap(value, (innerValue, innerKey) => ({ [innerKey]: innerValue }));
-      return [...acc, `${'    '.repeat(depth)}${prefix}${key}: ${stylishFormatter(newValue, depth + 1)}`];
-    }
-    return [...acc, `${'    '.repeat(depth)}${prefix}${key}: ${value}`];
+    const newValue = Array.isArray(value)
+      ? value
+      : _.flatMap(value, (innerValue, innerKey) => ({ [innerKey]: innerValue }));
+    return [...acc, `${margin}${prefix}${key}: ${stylishFormatter(newValue, depth + 1)}`];
   }, []);
-  return `{\n${mapped.join('\n')}\n${'    '.repeat(depth)}}`;
+  return `{\n${mapped.join('\n')}\n${margin}}`;
 };
 export default stylishFormatter;
