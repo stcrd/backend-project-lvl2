d35c5a9a2e2d1672ade4e71c1c6d646ccf2c8081
stcrd <nurzhan82@mail.ru>
2021-04-18 23:35:35 +0600

    delete unnecessary formatter from src/formatters/index.js

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,5 +1,4 @@
-import defaultFormatter from './default-formatter.js';
 import stylish from './stylish.js';
 import plain from './plain.js';
 
-export default (format) => ({ defaultFormatter, stylish, plain }[format]);
+export default (format) => ({ stylish, plain }[format]);
