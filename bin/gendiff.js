3f478e44e10dcd62dc5f8de9dd0b8fffd3a0f2d2
stcrd <nurzhan82@mail.ru>
2021-04-18 23:34:54 +0600

    set stylish as default formatter in executable

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -8,7 +8,7 @@ const { program } = commander;
 program
   .version('0.0.1')
   .description('Compares two configuration files and shows the difference.')
-  .option('-f, --format [type]', 'output format', 'defaultFormatter')
+  .option('-f, --format [type]', 'output format', 'stylish')
   .arguments('<filepath1> <filepath2>')
   .action((filepath1, filepath2) => {
     const diff = genDiff(filepath1, filepath2);
