c44a35b386882315d6aa16a1a64cee741150182c
stcrd <nurzhan82@mail.ru>
2021-04-23 00:32:03 +0600

    another refactor of stylish

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,20 +1,26 @@
 import _ from 'lodash';
 
 const stylish = (diff) => {
+  const getPrefix = (el) => ({ deleted: '  - ', added: '  + ' }[el.status] ?? '    ');
+  const getProperValue = (value) => {
+    if (!(value instanceof Object)) {
+      return value;
+    }
+    const newValue = Array.isArray(value)
+      ? value
+      : _.map(value, (innerValue, innerKey) => ({ [innerKey]: innerValue }));
+    return newValue;
+  };
+
   const iter = (entries, depth = 0) => {
+    if (!Array.isArray(entries)) return `${entries}`;
     const margin = '    '.repeat(depth);
-    const getPrefix = (el) => ({ deleted: '  - ', added: '  + ' }[el.status] ?? '    ');
     const mapped = entries.map((el) => {
       const [[key, value]] = Object.entries(el);
       const prefix = getPrefix(el);
       const keyPart = `${margin}${prefix}${key}`;
-      if (!(value instanceof Object)) {
-        return `${keyPart}: ${value}`;
-      }
-      const newValue = Array.isArray(value)
-        ? value
-        : _.map(value, (innerValue, innerKey) => ({ [innerKey]: innerValue }));
-      return `${keyPart}: ${iter(newValue, depth + 1)}`;
+      const properValue = getProperValue(value);
+      return `${keyPart}: ${iter(properValue, depth + 1)}`;
     });
     return `{\n${mapped.join('\n')}\n${margin}}`;
   };
