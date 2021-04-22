b4e9b2cc85a83fa376ca7f864c6a0003c4771d3e
stcrd <nurzhan82@mail.ru>
2021-04-22 23:47:46 +0600

    refactor stylish: reduce > map

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -2,15 +2,18 @@ import _ from 'lodash';
 
 const stylish = (diff, depth = 0) => {
   const margin = '    '.repeat(depth);
-  if (!(diff instanceof Object)) return diff;
-  const mapped = diff.reduce((acc, el) => {
+  const mapped = diff.map((el) => {
     const [[key, value]] = Object.entries(el);
-    const prefix = { deleted: '  - ', added: '  + ' }[el.status] || '    ';
-    const newValue = (Array.isArray(value) || !(value instanceof Object))
+    const prefix = { deleted: '  - ', added: '  + ' }[el.status] ?? '    ';
+    const keyPart = `${margin}${prefix}${key}`;
+    if (!(value instanceof Object)) {
+      return `${keyPart}: ${value}`;
+    }
+    const newValue = Array.isArray(value)
       ? value
-      : _.flatMap(value, (innerValue, innerKey) => ({ [innerKey]: innerValue }));
-    return [...acc, `${margin}${prefix}${key}: ${stylish(newValue, depth + 1)}`];
-  }, []);
+      : _.map(value, (innerValue, innerKey) => ({ [innerKey]: innerValue }));
+    return `${keyPart}: ${stylish(newValue, depth + 1)}`;
+  });
   return `{\n${mapped.join('\n')}\n${margin}}`;
 };
 export default stylish;
