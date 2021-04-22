e74b60cea745cb815fc309b3de627a5da52418a2
stcrd <nurzhan82@mail.ru>
2021-04-23 00:08:05 +0600

    refactor stylish: add aux funcion

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,19 +1,23 @@
 import _ from 'lodash';
 
-const stylish = (diff, depth = 0) => {
-  const margin = '    '.repeat(depth);
-  const mapped = diff.map((el) => {
-    const [[key, value]] = Object.entries(el);
-    const prefix = { deleted: '  - ', added: '  + ' }[el.status] ?? '    ';
-    const keyPart = `${margin}${prefix}${key}`;
-    if (!(value instanceof Object)) {
-      return `${keyPart}: ${value}`;
-    }
-    const newValue = Array.isArray(value)
-      ? value
-      : _.map(value, (innerValue, innerKey) => ({ [innerKey]: innerValue }));
-    return `${keyPart}: ${stylish(newValue, depth + 1)}`;
-  });
-  return `{\n${mapped.join('\n')}\n${margin}}`;
+const stylish = (diff) => {
+  const iter = (entries, depth = 0) => {
+    const margin = '    '.repeat(depth);
+    const getPrefix = (el) => ({ deleted: '  - ', added: '  + ' }[el.status] ?? '    ');
+    const mapped = entries.map((el) => {
+      const [[key, value]] = Object.entries(el);
+      const prefix = getPrefix(el);
+      const keyPart = `${margin}${prefix}${key}`;
+      if (!(value instanceof Object)) {
+        return `${keyPart}: ${value}`;
+      }
+      const newValue = Array.isArray(value)
+        ? value
+        : _.map(value, (innerValue, innerKey) => ({ [innerKey]: innerValue }));
+      return `${keyPart}: ${iter(newValue, depth + 1)}`;
+    });
+    return `{\n${mapped.join('\n')}\n${margin}}`;
+  };
+  return iter(diff);
 };
 export default stylish;
