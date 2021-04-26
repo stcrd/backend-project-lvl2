f746820fb08ccda8827c82ea5d5e7385967387ed
stcrd <nurzhan82@mail.ru>
2021-04-27 01:28:25 +0600

    refactor generateDifference using sets

--- src/generateDifference.js
+++ src/generateDifference.js
@@ -1,29 +1,27 @@
 import _ from 'lodash';
 import parse from './parsers.js';
 
-const isObject = (value) => value instanceof Object && !Array.isArray(value);
-const areBothObjects = (value1, value2) => isObject(value1) && isObject(value2);
-
 const generateDiff = (data1, data2) => {
-  const allKeys = _.sortedUniq(_.sortBy([...Object.keys(data1), ...Object.keys(data2)]));
-  const sharedKeys = _.intersection(Object.keys(data1), Object.keys(data2));
-  const mapped = allKeys.flatMap((key) => {
-    const value1 = data1[key];
-    const value2 = data2[key];
-    if (value1 === value2) {
-      return { [key]: value1 };
-    }
-    if (areBothObjects(value1, value2)) {
+  const keys1 = Object.keys(data1);
+  const keys2 = Object.keys(data2);
+  const mapShared = (key, obj1, obj2) => {
+    const value1 = obj1[key];
+    const value2 = obj2[key];
+    if (value1 === value2) return { [key]: value1 };
+    if (value1 instanceof Object && value2 instanceof Object) {
       return { [key]: generateDiff(value1, value2) };
     }
-    if (sharedKeys.includes(key)) {
-      return [{ [key]: value1, status: 'deleted' }, { [key]: value2, status: 'added' }];
-    }
-    return value1
-      ? { [key]: value1, status: 'deleted' }
-      : { [key]: value2, status: 'added' };
+    return [{ [key]: value1, status: 'deleted' }, { [key]: value2, status: 'added' }];
+  };
+  const inBoth = _.intersection(keys1, keys2).flatMap((key) => mapShared(key, data1, data2));
+  const removed = _.difference(keys1, keys2).map((key) => ({ [key]: data1[key], status: 'deleted' }));
+  const added = _.difference(keys2, keys1).map((key) => ({ [key]: data2[key], status: 'added' }));
+  const merged = [...inBoth, ...removed, ...added];
+  const sorted = _.sortBy(merged, (entry) => {
+    const [key] = Object.keys(entry);
+    return key;
   });
-  return mapped;
+  return sorted;
 };
 
 export default (filepath1, filepath2) => generateDiff(parse(filepath1), parse(filepath2));
