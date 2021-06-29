9ea61ac526084c59f297f7c28a4a216c83c7d7ed
stcrd <nurzhan82@mail.ru>
2021-06-29 20:23:48 +0600

    refactor plain formatter

--- src/formatters/json.js
+++ src/formatters/json.js
@@ -1,1 +1,15 @@
-export default (diff) => diff;
+const jsonFormatter = (diff) => {
+  const reduced = diff.reduce((acc, el) => {
+    const keys = Object.keys(el);
+    const [key] = keys;
+    const value = el[key];
+    const properValue = Array.isArray(el[key]) ? jsonFormatter(value) : value;
+    const valueIfStatusExists = keys.includes('status') ? { [el.status]: properValue } : properValue;
+    const newEl = acc[key]
+      ? { [key]: { ...acc[key], ...valueIfStatusExists } }
+      : { [key]: valueIfStatusExists };
+    return { ...acc, ...newEl };
+  }, {});
+  return reduced;
+};
+export default jsonFormatter;
